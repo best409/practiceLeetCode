5c6ea19612a10c972e83070449d29f214d84dbfa
范帅兵 <sinfan@yeah.net>
2020-07-27 20:50:20 +0800

    [2020-07-27] [简单:100 相同的树  ]

---
+++ fanshuaibing/50 easy algorithms/leetcode/editor/cn/[100]相同的树.java
@@ -0,0 +1,68 @@
+//给定两个二叉树，编写一个函数来检验它们是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 示例 1: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+// 
+//
+// 示例 3: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 406 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // 都为空的话，显然相同
+        if (p == null && q == null) return true;
+        // 一个为空，一个非空，显然不同
+        if (p == null || q == null) return false;
+        // 两个都非空，但 val 不一样也不行
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
